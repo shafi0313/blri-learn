16bb99d939c3a70464c9a82af73b176b6b5b1094
shafi1997 <msh.shafiul@gmail.com>
2023-04-03 15:45:12 +0600

    mail

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
+    Click the Reset Password button to set a new password.
 
-Click the Reset Password button to set a new password.
+    @component('mail::button', ['url' => route('resetPassword', ['token' => $forgetData->token])])
+        Reset Password
+    @endcomponent
 
-@component('mail::button', ['url' => route('resetPassword', ['token' => $forgetData->token])])
-Reset Password
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
+    
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
