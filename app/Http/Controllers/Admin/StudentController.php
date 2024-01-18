e9eb43062eeb5b29a2848b3192360f8e3e84a7e8
shafi0313 <msh.shafiul@gmail.com>
2024-01-18 20:40:33 +0600

    Add DataTables functionality to StudentController list method and update student list view

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers\Admin;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
 
 class StudentController extends Controller
 {
-    public function list()
+    public function list(Request $request)
     {
-        $students = User::select('id', 'name', 'email', 'phone', 'gender', 'district_id', 'address')->with([
-            'district' => fn ($q) => $q->select('id', 'name')
-        ])->wherePermission('2')->get();
-        return view('admin.student.list', compact('students'));
+        if ($request->ajax()) {
+            $students = User::select('id', 'name', 'email', 'phone', 'gender', 'district_id', 'address')->with([
+                'district' => fn ($q) => $q->select('id', 'name')
+            ])->wherePermission('2')->get();
+            return DataTables::eloquent($students)
+                ->addIndexColumn()
+                ->rawColumns(['status', 'action'])
+                ->make(true);
+        }
+        return view('admin.student.list');
     }
 }
